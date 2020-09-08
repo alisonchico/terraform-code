eaf3fc24134a18a2180a406297da1931b2a32a16
achicoria <alison.chico@gmail.com>
2020-09-08 20:43:45 -0300

    push

--- aws-code/service-add/main.tf
+++ aws-code/service-add/main.tf
@@ -1,7 +1,7 @@
 #Inputs Generated at Infra Module
 module "serviceadd" {
 source = "./add-service"
-OAuthToken = "904e888f310f86acea1466a9e89079e35b2bb06c"
+OAuthToken = "personaltoken"
 Owner  = "devopsrajpurohit"
 Repo   = "NexSimpleDockerWebApp"
 branch = "master"
@@ -10,15 +10,15 @@ containerPort = "80"
 health_check = "/health"
 
 #Need to Change - Inputs for Service-ADD
-RDS-HOST = "nexwebapp.cluster-cne3z6hmyfpn.us-west-1.rds.amazonaws.com"
+RDS-HOST = "nexwebapp.cluster-idn.us-west-1.rds.amazonaws.com"
 RDS-username = "nexwebapp"
 RDS_Password = "nexwebapp"
-Redis_Host = "nexwebapp.60h3uj.0001.usw1.cache.amazonaws.com"
+Redis_Host = "nexwebapp.id.0001.usw1.cache.amazonaws.com"
 servicename = "applicationgm1"
 path= "privacy"
-listener_arn ="arn:aws:elasticloadbalancing:us-west-1:061520891697:listener/app/nexwebapp/889dc0504eba576e/8a6d98a2cf02dbba"
+listener_arn ="arn:aws:elasticloadbalancing:us-west-1:id:listener/app/nexwebapp/889dc0504eba576e/8a6d98a2cf02dbba"
 cluster_name = "nexwebapp"
-ecs_cluster_id = "arn:aws:ecs:us-west-1:061520891697:cluster/nexwebapp"
+ecs_cluster_id = "arn:aws:ecs:us-west-1:id:cluster/nexwebapp"
 location = "nexwebapp-artifacts-tbxvga0y"
 security_groups = [
   "sg-02995e87e5dd8c060",
